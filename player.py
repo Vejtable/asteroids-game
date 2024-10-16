4f0cddf51ecdcd79750304725cfb13d00fd540ec
Vejtable <chefgraham93@gmail.com>
2024-10-16 17:17:01 -0400

    add forward and backward movement

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -13,7 +13,12 @@ class Player(CircleShape):
         if keys[pygame.K_a]:
             self.rotate(dt)
         if keys[pygame.K_d]:
-            self.rotate(-dt)  
+            self.rotate(-dt)
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
 
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -22,6 +27,10 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+    
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
     
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
