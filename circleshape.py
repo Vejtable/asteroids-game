97d24a44f2238e59424d309ef94ba6f38548a60e
Vejtable <chefgraham93@gmail.com>
2024-11-09 14:43:25 -0500

    Added player/asteroid collision

--- circleshape.py
+++ circleshape.py
@@ -6,7 +6,6 @@ class CircleShape(pygame.sprite.Sprite):
             super().__init__(self.containers)
         else:
             super().__init__()
-
         self.position = pygame.Vector2(x, y)
         self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
@@ -16,3 +15,6 @@ class CircleShape(pygame.sprite.Sprite):
 
     def update(self, dt):
         pass
+
+    def collides_with(self, other):
+        return self.position.distance_to(other.position) <= self.radius + other.radius
